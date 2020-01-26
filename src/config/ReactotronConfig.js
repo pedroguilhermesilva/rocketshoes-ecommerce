25e22111b90faec3d421b13a656ebd70b2ae1325
Pedro <pedroguilheermesilva@hotmail.com>
2020-01-26 16:21:59 -0300

    finished redux saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactontron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactontron.configure()
     .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect();
 
   tron.clear();
